37ec349c8e1643d168f369a09f7530bd88418809
skurisu11 <s-kurisu@nixe-inc.com>
2023-08-02 06:11:12 +0900

    huskyTest

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-ons-page>
+  <v-ons-page> <!-- インデントがない -->
     <v-ons-toolbar>
-      <div class="center">{{ title }}</div>
+      <div class="center">{{ title }}</div> <!-- インデントが不足している -->
       <div class="right">
-        <v-ons-toolbar-button>
+        <v-ons-toolbar-button> <!-- インデントが多すぎる -->
           <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
         </v-ons-toolbar-button>
       </div>
     </v-ons-toolbar>
-    <div style="text-align: center; padding-top:10px">Hello World!</div>
-    <p style="text-align: center">
-      <v-ons-button @click="alert">Click Me!</v-ons-button>
+    <div style="text-align:center;padding-top:10px">Hello World!</div> <!-- スペースが不足している -->
+    <p style="text-align:center">
+      <v-ons-button@click="alert">Click Me!</v-ons-button> <!-- スペースがない -->
     </p>
   </v-ons-page>
 </template>
